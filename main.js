6f262f35d105069113280e79e2c1bb2b51f6aa49
Nathan Riemer <nathan.riemer@gmail.com>
2015-11-04 13:59:43 -0500

    End of Act 2

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var $prompt = $('#prompt');
 var $log = $('#log');
 var purse = 10000;
+var loans = {};
 
 
 var addLI = function(liText, liClass) {
@@ -11,9 +12,32 @@ var addLI = function(liText, liClass) {
 };
 
 var parsePrompt = function(promptText) {
-	if (promptText === 'purse') {
-		var response = 'ShylockBot pulls out his purse containing ' + purse + ' ducats';
-		addLI(response, 'action');
+	if (promptText.indexOf(' ') === -1) {
+		var cmd = promptText;
+	} else {
+		var promptArray = promptText.split(' ')
+		var cmd = promptArray[0];
+	}
+
+	switch (cmd) {
+		case 'purse':
+			var response = 'ShylockBot pulls out his purse containing ' + purse + ' ducats';
+			addLI(response, 'action');
+			break;
+		case 'loan':
+			var person = promptArray[1];
+			var amount = parseInt(promptArray[2]);
+			if (loans[person]) {
+				loans[person] += amount;
+			} else {
+				loans[person] = amount;
+			}
+			purse -= amount;
+			console.log(loans);
+			var response = 'ShylockBot gives ' + person + ' ' + amount + ' ducats';
+			addLI(response, 'action');
+		default:
+			break;
 	}
 };
 
